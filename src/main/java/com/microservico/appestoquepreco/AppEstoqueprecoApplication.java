a32b7e6d6b5972b62672c6e8e1c3b09878fd47b5
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-06 19:23:52 -0300

    initial commit

---
+++ src/main/java/com/microservico/appestoquepreco/AppEstoqueprecoApplication.java
@@ -0,0 +1,13 @@
+package com.microservico.appestoquepreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEstoqueprecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEstoqueprecoApplication.class, args);
+	}
+
+}
