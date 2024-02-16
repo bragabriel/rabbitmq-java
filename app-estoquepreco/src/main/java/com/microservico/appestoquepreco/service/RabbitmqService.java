f578994f369a0bc45c05f4b9afeb09ac34f74aa5
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-16 17:30:11 -0300

    refactor: sending serialized messages, so they can be read by other microservices

--- app-estoquepreco/src/main/java/com/microservico/appestoquepreco/service/RabbitmqService.java
+++ app-estoquepreco/src/main/java/com/microservico/appestoquepreco/service/RabbitmqService.java
@@ -1,5 +1,6 @@
 package com.microservico.appestoquepreco.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,16 @@ public class RabbitmqService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     //Envia mensagem ao RabbitMQ
-    public void enviaMensagem(String nomeFila, Object mensagem){
-        this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
+    public void enviaMensagem(String nomeFila, Object mensagem){ //Serializar para poder utilizar por qualquer microserviço (java, node, .net...)
+        try{
+            String mensagemJson = this.objectMapper.writeValueAsString(mensagem); //transformando em json
+            this.rabbitTemplate.convertAndSend(nomeFila, mensagemJson);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
